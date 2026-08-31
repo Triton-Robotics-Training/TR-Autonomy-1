c6ddec4b4df6a81aa4540d99f32653aecfd40ce7
Chase <chase001cz@gmail.com>
2026-08-30 22:59:17 -0700

    Chase's commit for position prediction

--- src/your_solution/src/spin_sol.cpp
+++ src/your_solution/src/spin_sol.cpp
@@ -1,5 +1,10 @@
 #include "spin_sol.h"
+#include <chrono>
+#include <functional>
+#include <memory>
 
+#include "rclcpp/rclcpp.hpp"
+using std::placeholders::_1;
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<SpinSolution>());
@@ -11,4 +16,38 @@ int main(int argc, char *argv[]) {
 SpinSolution::SpinSolution() : Node("spinsolution") {
   RCLCPP_INFO(this->get_logger(), "Remove this statement from spin_sol.cpp");
   // your code here
+  estimate_publisher = this->create_publisher<ArrayMsg>("predictedpos",10);
+  pos_sub = this->create_subscription<ArrayMsg>(
+    "measuredpos", 10, std::bind(&SpinSolution::pos_callback, this, _1)
+  );
+  vel_sub = this->create_subscription<ArrayMsg>(
+    "measuredvel", 10, std::bind(&SpinSolution::vel_callback, this, _1)
+  );
+
+  timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(500), std::bind(&SpinSolution::est_pub, this));
+  
 }
+
+void SpinSolution::pos_callback(const ArrayMsg::SharedPtr msg){
+  RCLCPP_INFO(this->get_logger(),"Position: %f",msg->data[0]);
+  cur_pos = msg->data;
+}
+
+void SpinSolution::vel_callback(const ArrayMsg::SharedPtr msg)
+{
+  RCLCPP_INFO(this->get_logger(),"Velocity: %f",msg->data[0]);
+  cur_vel = msg->data;
+}
+
+void SpinSolution::est_pub()
+{
+  for(std::size_t  i = 0; i<cur_pos.size(); i++){
+    cur_pos[i] += cur_vel[i]/3;
+  }
+  ArrayMsg msg_out;
+  msg_out.data = cur_pos;
+  estimate_publisher -> publish(msg_out);
+  RCLCPP_INFO(this->get_logger(),"Pub %f | %f",cur_pos[0],cur_pos[1]);
+}
+
--- src/your_solution/src/spin_sol.h
+++ src/your_solution/src/spin_sol.h
@@ -1,15 +1,25 @@
 #ifndef YOUR_SOLUTION_SRC_SPIN_SOL_H_
 #define YOUR_SOLUTION_SRC_SPIN_SOL_H_
 
+#include <vector>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float64_multi_array.hpp"
 using ArrayMsg = std_msgs::msg::Float64MultiArray;
 
 class SpinSolution : public rclcpp::Node {
  public:
-  SpinSolution();
+    SpinSolution();
  private:
     // your code here
+    rclcpp::Subscription<ArrayMsg>::SharedPtr pos_sub;
+    rclcpp::Subscription<ArrayMsg>::SharedPtr vel_sub;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<ArrayMsg>::SharedPtr estimate_publisher;
+    std::vector<double> cur_pos = {0.0,0.0};
+    std::vector<double> cur_vel = {0.0,0.0};
+    void pos_callback(const ArrayMsg::SharedPtr msg);
+    void vel_callback(const ArrayMsg::SharedPtr msg);
+    void est_pub();
 };
 
 #endif //YOUR_SOLUTION_SRC_SPIN_SOL_H_
