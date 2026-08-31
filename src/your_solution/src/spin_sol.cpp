30158d343129b1c7368359c16ee3446e8119a8ec
Chase <chase001cz@gmail.com>
2026-08-30 23:12:31 -0700

    get clangd workign

--- src/your_solution/src/spin_sol.cpp
+++ src/your_solution/src/spin_sol.cpp
@@ -12,42 +12,36 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-//your code here
+// your code here
 SpinSolution::SpinSolution() : Node("spinsolution") {
   RCLCPP_INFO(this->get_logger(), "Remove this statement from spin_sol.cpp");
   // your code here
-  estimate_publisher = this->create_publisher<ArrayMsg>("predictedpos",10);
+  estimate_publisher = this->create_publisher<ArrayMsg>("predictedpos", 10);
   pos_sub = this->create_subscription<ArrayMsg>(
-    "measuredpos", 10, std::bind(&SpinSolution::pos_callback, this, _1)
-  );
+      "measuredpos", 10, std::bind(&SpinSolution::pos_callback, this, _1));
   vel_sub = this->create_subscription<ArrayMsg>(
-    "measuredvel", 10, std::bind(&SpinSolution::vel_callback, this, _1)
-  );
+      "measuredvel", 10, std::bind(&SpinSolution::vel_callback, this, _1));
 
-  timer_ = this->create_wall_timer(
-      std::chrono::milliseconds(500), std::bind(&SpinSolution::est_pub, this));
-  
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(500),
+                                   std::bind(&SpinSolution::est_pub, this));
 }
 
-void SpinSolution::pos_callback(const ArrayMsg::SharedPtr msg){
-  RCLCPP_INFO(this->get_logger(),"Position: %f",msg->data[0]);
+void SpinSolution::pos_callback(const ArrayMsg::SharedPtr msg) {
+  RCLCPP_INFO(this->get_logger(), "Position: %f", msg->data[0]);
   cur_pos = msg->data;
 }
 
-void SpinSolution::vel_callback(const ArrayMsg::SharedPtr msg)
-{
-  RCLCPP_INFO(this->get_logger(),"Velocity: %f",msg->data[0]);
+void SpinSolution::vel_callback(const ArrayMsg::SharedPtr msg) {
+  RCLCPP_INFO(this->get_logger(), "Velocity: %f", msg->data[0]);
   cur_vel = msg->data;
 }
 
-void SpinSolution::est_pub()
-{
-  for(std::size_t  i = 0; i<cur_pos.size(); i++){
-    cur_pos[i] += cur_vel[i]/3;
+void SpinSolution::est_pub() {
+  for (std::size_t i = 0; i < cur_pos.size(); i++) {
+    cur_pos[i] += cur_vel[i] / 3;
   }
   ArrayMsg msg_out;
   msg_out.data = cur_pos;
-  estimate_publisher -> publish(msg_out);
-  RCLCPP_INFO(this->get_logger(),"Pub %f | %f",cur_pos[0],cur_pos[1]);
+  estimate_publisher->publish(msg_out);
+  RCLCPP_INFO(this->get_logger(), "Pub %f | %f", cur_pos[0], cur_pos[1]);
 }
-
